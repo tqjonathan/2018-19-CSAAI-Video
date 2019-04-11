995911600fafdfa763c8d93c3f63cfa15512c6ce
tqjonathan <ja.toctaguano@alumnos.urjc.es>
2019-04-11 13:56:54 +0200

    commit

--- realizador.js
+++ realizador.js
@@ -2,16 +2,20 @@ function main() {
   console.log("video1 prueba");
 
   video1 = document.getElementById('video1');
-
   video1.width = 300;
   video1.height = 200;
 
-  ver = document.getElementById('ver');
+  video2 = document.getElementById('video2');
+  video2.width = 300;
+  video2.height = 200;
 
-  ver.onclick = () => {
-    console.log("Ver!")
+  video3 = document.getElementById('video3');
+  video3.width = 300;
+  video3.height = 200;
 
-    //-- Indicar la fuente del vídeo
-    video1.src ="Video1.mp4"
-  }
+
+
+
 }
+
+// IDEA:
