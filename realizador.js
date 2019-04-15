58408b1acce559d8e2204ce1934426d2f1b3d9b3
tqjonathan <ja.toctaguano@alumnos.urjc.es>
2019-04-15 21:47:02 +0200

    commit

--- realizador.js
+++ realizador.js
@@ -1,5 +1,5 @@
 function main() {
-  console.log("video1 prueba");
+  console.log("Empezamos");
 
   video1 = document.getElementById('video1');
   video1.width = 300;
@@ -13,9 +13,88 @@ function main() {
   video3.width = 300;
   video3.height = 200;
 
+  principal = document.getElementById('principal')
+  principal.width = 500;
+  principal.height = 400;
 
+  // IDEA: botones de play
 
+  ver1 = document.getElementById('play1');
+  ver2 = document.getElementById('play2');
+  ver3 = document.getElementById('play3');
+
+  video1.load();
+
+// IDEA: Pantalla principal
+
+  ver1.onclick = () => {
+    console.log("repoduciendo video1");
+
+    principal.src = "Video1.mp4";
+    principal.currentTime = video1.currentTime
+    principal.play();
+  }
+
+  ver2.onclick = () => {
+    console.log("repoduciendo video2");
+
+    principal.src = "Video2.mp4";
+    principal.currentTime = video2.currentTime
+    principal.play();
+  }
+
+  ver3.onclick = () => {
+    console.log("repoduciendo video3");
+
+    principal.src = "Video3.mp4";
+    principal.currentTime = video3.currentTime
+    principal.play();
+  }
+
+  // IDEA: Sonido On/Off
+
+  video1.onmouseover = () => {
+      console.log("Audio 1 On")
+    video1.muted= false;
+  }
+  video1.onmouseout = () => {
+    console.log("Audio 1 Off")
+    video1.muted= true;
+  }
+
+  video2.onmouseover = () => {
+    console.log("Audio 2 On")
+    video2.muted= false;
+  }
+  video2.onmouseout = () => {
+    console.log("Audio 2 Off")
+    video2.muted= true;
+  }
+
+  video3.onmouseover = () => {
+    console.log("Audio 3 On")
+    video3.muted= false;
+  }
+  video3.onmouseout = () => {
+    console.log("Audio 3 Off")
+    video3.muted= true;
+  }
+
+// IDEA: Loop
+
+  bucle = document.getElementById("bucle");
+
+  bucle.onclick = () => {
+    if (principal.loop == true) {
+      principal.loop = false;
+      alert("Loop Desactivado");
+      console.log("loop off");
+    } else {
+      principal.loop = true;
+      alert("Loop Activado");
+      console.log("loop on");
+    }
+
+  }
 
 }
-
-// IDEA:
